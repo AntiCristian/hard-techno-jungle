b127fc04080ba3325e20cf5f073b9783e8e8229a
AntiCristian <kr1st14nsk8@gmail.com>
2025-04-27 13:43:07 -0500

    chore: initial project setup with Spring Boot and Angular

---
+++ backend/src/main/java/dev/monje/hard_techno_jungle/HardTechnoJungleApplication.java
@@ -0,0 +1,13 @@
+package dev.monje.hard_techno_jungle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardTechnoJungleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HardTechnoJungleApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/monje/hard_techno_jungle/HardTechnoJungleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.monje.hard_techno_jungle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardTechnoJungleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
